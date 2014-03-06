d753ee6dde928ec5f6b92878bdaec44d3e842c87
Michael Dresser <michaeldresser@darkminiserver.local>
2014-03-06 12:12:13 -0800

    Added missing prototypes to the table view class

--- MDTableView.h
+++ MDTableView.h
@@ -3,5 +3,22 @@
 #import <UIKit/UIKit.h>
 
 @interface MDTableView : UITableView
-
+/**
+ *  <#Description#>
+ *
+ *  @param color <#color description#>
+ */
+- (void) setExtremeAreaColor:(UIColor *) color;
+/**
+ *  <#Description#>
+ *
+ *  @param color <#color description#>
+ */
+- (void) setTopAreaExtremeColor:(UIColor *) color;
+/**
+ *  <#Description#>
+ *
+ *  @param color <#color description#>
+ */
+- (void) setBottomAreaExtremeColor:(UIColor *) color;
 @end
