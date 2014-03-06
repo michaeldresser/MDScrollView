0bbd3b35d888b3797e09dadabc1dfcb812abdcd5
Michael Dresser <michaeldresser@darkminiserver.local>
2014-03-06 11:59:27 -0800

    Added tableview class

---
+++ MDTableView.m
@@ -0,0 +1,41 @@
+
+#import "MDTableView.h"
+
+@interface MDTableView ()
+@property (strong) UIView * extremeTop;
+@property (strong) UIView * extremeBottom;
+@end
+
+#define ExtremeAreaSize 400.0
+
+@implementation  MDTableView
+- (void) awakeFromNib
+{
+    [super awakeFromNib];
+    
+    self.extremeTop = [[UIView alloc] initWithFrame: CGRectMake(0, -ExtremeAreaSize, self.bounds.size.width, ExtremeAreaSize)];
+    [self.extremeTop setBackgroundColor: [UIColor whiteColor]];
+    [self addSubview: self.extremeTop];
+    
+    self.extremeBottom = [[UIView alloc] initWithFrame: CGRectMake(0, self.bounds.size.height, self.bounds.size.width, ExtremeAreaSize)];
+    [self.extremeBottom setBackgroundColor: [UIColor whiteColor]];
+    [self addSubview: self.extremeBottom];
+}
+
+- (void) setExtremeAreaColor:(UIColor *) color
+{
+    [self.extremeTop setBackgroundColor: color];
+    [self.extremeBottom setBackgroundColor: color];
+}
+
+- (void) setTopAreaExtremeColor:(UIColor *) color
+{
+    [self.extremeTop setBackgroundColor: color];
+}
+
+- (void) setBottomAreaExtremeColor:(UIColor *) color
+{
+    [self.extremeBottom setBackgroundColor: color];
+}
+
+@end
